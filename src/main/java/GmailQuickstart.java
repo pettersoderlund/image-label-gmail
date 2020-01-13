4fa7765d46fc01d27894e9219f64a5b9fee933f0
petsod <petter.soderlund@avanza.se>
2020-01-13 20:06:23 +0100

    initial commit

---
+++ src/main/java/GmailQuickstart.java
@@ -0,0 +1,238 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
+import com.google.api.services.gmail.model.MessagePart;
+import com.google.api.services.gmail.model.MessagePartBody;
+import com.google.common.collect.ImmutableList;
+import org.apache.commons.codec.binary.Base64;
+
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import java.io.*;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+
+// Imports the Google Cloud client library
+
+public class GmailQuickstart {
+    private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final String IMAGES_TEMP_PATH = "tmp_images";
+
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = ImmutableList.of(GmailScopes.GMAIL_LABELS, GmailScopes.GMAIL_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = GmailQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+
+    /**
+     * List all Messages of the user's mailbox matching the query.
+     *
+     * @param service Authorized Gmail API instance.
+     * @param userId User's email address. The special value "me"
+     * can be used to indicate the authenticated user.
+     * @param query String used to filter the Messages listed.
+     * @throws IOException
+     */
+    public static List<Message> listMessagesMatchingQuery(Gmail service, String userId,
+                                                          String query) throws IOException {
+        ListMessagesResponse response = service.users().messages().list(userId).setQ(query).execute();
+
+        List<Message> messages = new ArrayList<Message>();
+        while (response.getMessages() != null) {
+            messages.addAll(response.getMessages());
+            if (response.getNextPageToken() != null) {
+                String pageToken = response.getNextPageToken();
+                response = service.users().messages().list(userId).setQ(query)
+                        .setPageToken(pageToken).execute();
+            } else {
+                break;
+            }
+        }
+/*
+        for (Message message : messages) {
+            System.out.println(message.toPrettyString());
+        }
+*/
+        return messages;
+    }
+
+    public static void getAttachments(Gmail service, String userId, String messageId)
+            throws IOException {
+        Message message = service.users().messages().get(userId, messageId).execute();
+        List<MessagePart> parts = message.getPayload().getParts();
+        for (MessagePart part : parts) {
+            if (part.getFilename() != null && part.getFilename().length() > 0) {
+                String filename = part.getFilename();
+                String attId = part.getBody().getAttachmentId();
+                MessagePartBody attachPart = service.users().messages().attachments().
+                        get(userId, messageId, attId).execute();
+
+                Base64 base64Url = new Base64(true);
+                byte[] fileByteArray = base64Url.decodeBase64(attachPart.getData());
+                FileOutputStream fileOutFile =
+                        new FileOutputStream(IMAGES_TEMP_PATH+ "/" + filename);
+                fileOutFile.write(fileByteArray);
+                fileOutFile.close();
+            }
+        }
+    }
+
+    public static List<byte[]> getAttachmentsBytes(Gmail service, String userId, String messageId)
+            throws IOException {
+        Message message = service.users().messages().get(userId, messageId).execute();
+        List<MessagePart> parts = message.getPayload().getParts();
+        List<byte[]> attachmentImageByteArrays = new ArrayList<>();
+        for (MessagePart part : parts) {
+            // maybe check file ending if its a pic ?
+            if (part.getFilename() != null && part.getFilename().length() > 0) {
+                String filename = part.getFilename();
+                String attId = part.getBody().getAttachmentId();
+                MessagePartBody attachPart = service.users().messages().attachments().
+                        get(userId, messageId, attId).execute();
+
+                Base64 base64Url = new Base64(true);
+                byte[] fileByteArray = base64Url.decodeBase64(attachPart.getData());
+                attachmentImageByteArrays.add(fileByteArray);
+            }
+        }
+        return attachmentImageByteArrays;
+    }
+
+    public static List<EntityAnnotation> AnnotateImageWithGoogleVision(byte[] imgByteArray) throws IOException {
+
+    try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+        ByteString imgBytes = ByteString.copyFrom(imgByteArray);
+
+        // Builds the image annotation request
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
+        AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+                .addFeatures(feat)
+                .setImage(img)
+                .build();
+        requests.add(request);
+
+        // Performs label detection on the image file
+        BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+        List<AnnotateImageResponse> responses = response.getResponsesList();
+
+        //get first response (only sent in one image in the batch so should only be one response)
+        AnnotateImageResponse res = responses.get(0);
+
+        if (res.hasError()) {
+            System.out.printf("Error: %s\n", res.getError().getMessage());
+        }
+
+        return res.getLabelAnnotationsList();
+
+
+
+    }}
+
+
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // Print the labels in the user's account.
+        String user = "me";
+
+
+        // Find emails
+        List<Message> messages = listMessagesMatchingQuery(service, user, "subject:(Aktivitet vid din kamera) has:attachment after:2020/01/01 ");
+
+        // Process email
+        for (Message message : messages) {
+            //System.out.println(message.toPrettyString());
+            // download attachment
+            System.out.println("Downloading attachment!");
+            List<byte[]> imageByteArrays = getAttachmentsBytes(service, user, message.getId());
+
+            for (byte[] imageByteArray:imageByteArrays) {
+                System.out.println("Analyzing attachment!");
+                // Analyze attachment (google vision)
+                // req to set env var. GOOGLE_APPLICATION_CREDENTIALS
+                for (EntityAnnotation annotation:AnnotateImageWithGoogleVision(imageByteArray)){
+                    annotation.getAllFields().forEach((k, v) ->
+                            System.out.printf("%s : %s\n", k, v.toString()));
+                }
+            }
+
+
+        // Tag email with results?
+        // tag that analyzed email is analyzed. Include this tag in the search string above to avoid
+        // duplicate analyzes
+
+        // fin!
+
+        }
+
+        /*ListLabelsResponse listResponse = service.users().labels().list(user).execute();
+        List<Label> labels = listResponse.getLabels();
+        if (labels.isEmpty()) {
+            System.out.println("No labels found.");
+        } else {
+            System.out.println("Labels:");
+            for (Label label : labels) {
+                System.out.printf("- %s\n", label.getName());
+            }
+        }*/
+    }
+}
